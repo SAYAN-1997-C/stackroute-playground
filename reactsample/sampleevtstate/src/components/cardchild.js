004707dcf20b1bb3cf97ba5046b23db9eabd5770
Mary rani <mary.rani@stackroute.in>
2022-09-14 16:12:08 +0530

    events-hook

--- reactsample/sampleevtstate/src/components/cardchild.js
+++ reactsample/sampleevtstate/src/components/cardchild.js
@@ -1,13 +1,47 @@
+import { useState } from 'react';
 import './mystyle.css'
 
 
 export default function Cardchild(props)
 {
 
+    // declared a state variable named userdata , with initial value to be dummy
+
+   const[userdata, setUserdata]=useState("dummy");
+
+    let mydata="abc";
+  
+    const changeVal = (e)=>{
+            mydata="good day";
+            setUserdata("have a great day");
+            console.log(mydata);
+    }
+
+  const submitClick = (e) =>
+  {
+    props.onCardClick(userdata);
+  }
+
+
     return(
-    <div class="clscard">
+    <div className="clscard">
+          
           <h4> Card Display - Child </h4>
-           <h5> Value from Parent is {props.data} </h5>
+
+            {props.data}
+          <h5>  Normal constant value  {mydata} </h5>
+          <h5> State variable value {userdata} </h5>
+
+
+ <input type="text" onChange={ (evt)=>   setUserdata(evt.target.value) } />
+
+           {/* <h5> Value from Parent is {props.data} </h5> */}
+
+
+           <button onClick={changeVal}>Check State</button>
+
+           <button onClick={submitClick}>Submit</button>
+
     </div>
 
 
--- reactsample/sampleevtstate/src/components/dashboard.js
+++ reactsample/sampleevtstate/src/components/dashboard.js
@@ -8,30 +8,34 @@ class Dashboard extends React.Component
     constructor(props)
     {
         super(props);
-        this.state = {message:"welcome"}
+        this.state = {message:"welcome", datareceived:""}
    this.handleMyEvent=this.handleMyEvent.bind(this);
 
     }
 
-    handleMyEvent(event)
+    handleMyEvent(eventdata)
     {
-        this.setState( {message : event.target.value} );
-
-        console.log("text entered ");
+     console.log("Parent receiving data  : " + eventdata);
+     this.setState({datareceived:eventdata});
     }
 
     render()
     {
 return(
-<div class="clsdash">
-
+<div className="clsdash">
     <h1> Dashboard</h1>
+    
+     <h6> Data received from Card Child is :  {this.state.datareceived} </h6>
+    
+    {/* <h5> {this.state.message}</h5> */}
+    <input type="text" value={this.state.message} onChange={ (evt)=> this.setState( {message:evt.target.value})}/>
+    <Cardchild data={this.state.message} onCardClick={this.handleMyEvent} />
 
-    <h5> {this.state.message}</h5>
+    {/* <input type="text" value={this.state.message} onChange={this.handleMyEvent} /> */}
+
+
    
-    <Cardchild data={this.state.message} />
-
-    <input type="text" value={this.state.message} onChange={this.handleMyEvent} />
+<h5> End!!!!!!!</h5>
 
 
 </div>
